3b1629ff0cf81d17df57d6bf2798f4cc52ca242a
wittmana <wittmana@etsu.edu>
2020-12-03 23:03:33 -0700

    Added Mqtt functionality

--- PiCar.js
+++ PiCar.js
@@ -1,21 +1,6 @@
 const mqtt = require('mqtt');
 const Gpio = require('pigpio').Gpio;
 
-//Credentials for MQTT Server
-const options =
-{
-        port: '1883',
-        host: '192.168.0.16',
-        username: 'bucserve',
-        password: 'E+$uBu($'
-};
-
-//Connect to the broker
-client.on('connect', function ()
-{
-        console.log("\nController connected to broker.\n");
-});
-
 //Initiate motor controller
 const rightReverse = new Gpio(17, {mode: Gpio.OUTPUT});
 const leftFoward = new Gpio(27, {mode: Gpio.OUTPUT});
@@ -28,13 +13,71 @@ rightFoward.pwmWrite(0);
 leftFoward.pwmWrite(0);
 rightReverse.pwmWrite(0);
 
-setInterval(function()
+//Credentials for MQTT Server
+const options =
 {
-	//leftReverse.pwmWrite(200);
-	//rightFoward.pwmWrite(100);
-	//leftFoward.pwmWrite(100);
-	//rightReverse.pwmWrite(200);
+        port: '1883',
+        host: '192.168.0.16',
+        username: 'bucserve',
+        password: 'E+$uBu($'
+};
+
+//Connect to the server using the credentials
+const client = mqtt.connect(options);
+
+//Connect to the broker
+client.on('connect', function ()
+{
+        console.log("\nPi connected to broker.\n");
+		client.subscribe('sub', function (err)
+		{
+			if (err) throw "Error description: " + err;
+		});
+		client.subscribe('data', function (err)
+		{
+			if (err) throw "Error description: " + err;
+		});
 });
+
+client.on('message', async function(topic, message)
+{
+	if (topic == 'sub')
+	{
+		client.publish("ack", "", function (err)
+		{
+			if (err) throw "Error Description: " + err;
+		});	
+	}
+	if (topic == 'data')
+	{
+		if(message == 'foward')
+		{
+			console.log("Recieved Foward");
+			rightFoward.pwmWrite(255);
+			leftFoward.pwmWrite(255);
+			await sleep(1000);
+			rightFoward.pwmWrite(0);
+			leftFoward.pwmWrite(0);
+		}
+		else if(message == 'exit')
+		{
+			console.log("Recieved Exit");
+			leftReverse.pwmWrite(0);
+			rightFoward.pwmWrite(0);
+			rightReverse.pwmWrite(0);
+			leftFoward.pwmWrite(0);
+			delete rightReverse;
+			delete leftFoward;
+			process.exit(0);
+		}
+	}
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 process.on('SIGINT', function()
 {
---
+++ PiCarController.js
@@ -0,0 +1,103 @@
+/*Name:		Alexander Wittman
+* Project:  Final IoT Project
+* Due Date:	12/09/2020
+*/
+
+//Packages required to recieve data from the broker
+const mqtt = require('mqtt');
+
+//Allow input from the user
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.pause();
+
+//Determine if the controller is connected to the Car
+var isConnected = false;
+
+//Credentials required to connect to the broker
+const options = 
+{
+	port: '1883',
+	host: '192.168.0.16',
+	username: 'bucserve',
+	password: 'E+$uBu($'
+};
+
+//Connect to the server using the credentials
+const client = mqtt.connect(options);
+
+//Connect to the broker and subscribe to 'data'
+client.on('connect', function ()
+{
+	console.log("\nController connected to broker.\n")
+	client.subscribe('ack', function (err)
+	{
+		if (err) throw "Error description: " + err;
+	});
+});
+
+async function connection()
+{
+	isConnected = false;
+	client.publish("sub", "", function (err)
+	{
+		if (err) throw "Error Description: " + err;
+	});
+	
+	await sleep(1000);
+	
+	if (isConnected == false)
+	{
+		process.stdin.pause();
+		console.log("Awaiting Connection");
+	}
+};
+
+setInterval (function()
+{
+	connection();
+},10000);
+
+client.on('message', function(topic, message)
+{
+	if(topic == 'ack')
+	{
+		if(isConnected == false)
+		{
+			console.log("Connected");
+		}
+		isConnected = true;
+		process.stdin.resume();
+	}
+});
+
+//Send data to the broker based on user input
+process.stdin.on('keypress', async function(key, data) 
+{
+	if (data.name === 'w')
+	{
+		client.publish("data", "foward", function (err)
+        {
+			console.log("Sending W");
+            if (err) throw "Error Description: " + err;
+        });
+	}		
+	if (data.ctrl && data.name === 't')
+	{
+		client.publish("data", "exit", async function (err)
+		{
+			console.log("Sending Exit");
+			if (err) throw "Error Description: " + err;
+		});
+		console.log("Exiting");
+		await sleep(1000)
+		process.exit();
+	}
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
