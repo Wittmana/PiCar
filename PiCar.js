cd57ac32db66706d72d2be75e4efedeaf74cf8e5
wittmana <wittmana@etsu.edu>
2020-12-09 11:58:00 -0700

    Added Comments and encryption to the controller and the PiCar

--- PiCar.js
+++ PiCar.js
@@ -1,5 +1,6 @@
 const mqtt = require('mqtt');
 const Gpio = require('pigpio').Gpio;
+const NodeRSA = require('node-rsa');
 
 //Initiate motor controller
 const rightReverse = new Gpio(17, {mode: Gpio.OUTPUT});
@@ -12,6 +13,23 @@ leftReverse.pwmWrite(0);
 rightFoward.pwmWrite(0);
 leftFoward.pwmWrite(0);
 rightReverse.pwmWrite(0);
+
+//Create key for encryption: 1024 bit encryption
+const key = new NodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
+'MIICXAIBAAKBgQCJvh+nxi5ip+cv33Eumo/1Q/JYiBjKKoLV+dWQbYvx2FetP2Oh\n'+
+'/IQNFH/c1GtGaWwi0OCfGqNIUHmYhrVDN/h6d2Ap5w6WG/QQO3xo+JboK/Sed8iu\n'+
+'r6BrYa8wPkmlE3l6JEHLKY7sUXFGYnvz6cJRa7FvOLzUzzOHrZQh2B5WTQIDAQAB\n'+
+'AoGAPDvy9dg5YpUQCSRKB1o5x8R9zu88//NVIaNOHAl7oDe+J5nHxQd664liXLxb\n'+
+'aQAi9JqpD4qDitT+R4/o5ksskiUFuRaPvMXp8dyG3RxBvatU3cjHmvvkRpGyUtfa\n'+
+'udhJJV9ZY/lzFA8FP2NZYv0mgTsEsS3LeEiAY6T9Avd+l5UCQQDExqVFXV0XxKKQ\n'+
+'WSBNuuZ/KIU3D2fZ+AN70n7qqN/sLM1TK+HhN/4Du9N3N6t3eXMQ1CBH5CttR7Kd\n'+
+'AUNSCCjfAkEAszMIe8Zc/brAo7DDD6XCZ5LW6dUoU0QbM+FsEgokW3dSjFVUebf4\n'+
+'Z8xZNutLDxMU9JPl+B7hWsTkyGRrhUuqUwJAUdbwe+X1xuHgXrrCxbFbFj3LEfhV\n'+
+'UAFnqYMFiCFOaFLF3racjmgUPIdMzwOXS+x2H+SpvFy4dw3fyo7w6dg87wJAGbdg\n'+
+'J0sfOzZawOL0C0HRdUWoW+RAJjrm4HogJTKZZxrnmMYCGnmhR1wXDX+UxPQLFNJI\n'+
+'tWPRGVKaNnvaxkVGiwJBAJtYEcxaI6myXbVbMdQXxvH7WISV3KMyYqP8++BkkwCs\n'+
+'O5jBcbKf9MjnneMnlvR6PBqruWt+BVUwujvUXcuPoiM=\n'+
+'-----END RSA PRIVATE KEY-----');
 
 //Credentials for MQTT Server
 const options =
@@ -39,59 +57,79 @@ client.on('connect', function ()
 		});
 });
 
+//Process any topics/messages from the broker
 client.on('message', function(topic, message)
 {
+	//Decrypt message recieved from the broker
+	let decryptedMessage = key.decrypt(message);
+	
+	//If controller is requesting a response back, acknowledge
 	if (topic == 'sub')
 	{
-		client.publish("ack", "", function (err)
+		client.publish('ack', key.encrypt('ack'), function (err)
 		{
 			if (err) throw "Error Description: " + err;
 		});	
 	}
+	//If controller is sending data, process data
 	if (topic == 'data')
 	{
-		if (message == 'stop')
+		//Determine what the controller is sending for car control
+		if (decryptedMessage == 'stop')
 		{
+			//Stop, stop all motors
 			console.log("Recieved Stop")
 			stop();
 		}
-		else if(message == 'foward')
+		else if(decryptedMessage == 'foward')
 		{
+			//Foward, stop all motors
 			console.log("Recieved Foward");
 			stop();
+			//Set motors to go foward
 			rightFoward.pwmWrite(255);
 			leftFoward.pwmWrite(255);
 		}
-		else if(message == 'reverse')
+		else if(decryptedMessage == 'reverse')
 		{
-			console.log("Recieved Revers")
+			//Reverse, stop all motors
+			console.log("Recieved Reverse")
 			stop();
+			//Set all motors to go in reverse
 			rightReverse.pwmWrite(255);
 			leftReverse.pwmWrite(255);
 		}
-		else if(message == 'right')
+		else if(decryptedMessage == 'right')
 		{
+			//Right, stop all motors
 			console.log("Recieved Revers")
 			stop();
+			//Only spin the left motors to turn the PiCar right
 			leftFoward.pwmWrite(150);
 		}
-		else if(message == 'left')
+		else if(decryptedMessage == 'left')
 		{
+			//Left, stop all motors
 			console.log("Recieved Revers")
 			stop();
+			//Only spin the right motors to turn the PiCar left
 			rightFoward.pwmWrite(150);
 		}
-		else if(message == 'exit')
+		else if(decryptedMessage == 'exit')
 		{
+			//Exit, stop all motors
 			console.log("Recieved Exit");
 			stop();
+			//Delete the GPIO motor controls
 			delete rightReverse;
 			delete leftFoward;
+			//Exit Process
 			process.exit(0);
 		}
 	}
 });
 
+//Function to stop all motors
 function stop() {
 	leftReverse.pwmWrite(0);
 	rightFoward.pwmWrite(0);
@@ -99,10 +137,14 @@ function stop() {
 	leftFoward.pwmWrite(0);
 }
 
+//Kill process through ctrl+c
 process.on('SIGINT', function()
 {
+	//Stop all motors
 	stop();
+	//Delete the GPIO motor controls
 	delete rightReverse;
 	delete leftFoward;
+	//Exit process
 	process.exit(0);
 });
--- PiCarController.js
+++ PiCarController.js
@@ -5,6 +5,24 @@
 
 //Packages required to recieve data from the broker
 const mqtt = require('mqtt');
+const NodeRSA = require('node-RSA');
+
+//Create key for encryption: 1024 bit encryption
+const key = new NodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
+'MIICXAIBAAKBgQCJvh+nxi5ip+cv33Eumo/1Q/JYiBjKKoLV+dWQbYvx2FetP2Oh\n'+
+'/IQNFH/c1GtGaWwi0OCfGqNIUHmYhrVDN/h6d2Ap5w6WG/QQO3xo+JboK/Sed8iu\n'+
+'r6BrYa8wPkmlE3l6JEHLKY7sUXFGYnvz6cJRa7FvOLzUzzOHrZQh2B5WTQIDAQAB\n'+
+'AoGAPDvy9dg5YpUQCSRKB1o5x8R9zu88//NVIaNOHAl7oDe+J5nHxQd664liXLxb\n'+
+'aQAi9JqpD4qDitT+R4/o5ksskiUFuRaPvMXp8dyG3RxBvatU3cjHmvvkRpGyUtfa\n'+
+'udhJJV9ZY/lzFA8FP2NZYv0mgTsEsS3LeEiAY6T9Avd+l5UCQQDExqVFXV0XxKKQ\n'+
+'WSBNuuZ/KIU3D2fZ+AN70n7qqN/sLM1TK+HhN/4Du9N3N6t3eXMQ1CBH5CttR7Kd\n'+
+'AUNSCCjfAkEAszMIe8Zc/brAo7DDD6XCZ5LW6dUoU0QbM+FsEgokW3dSjFVUebf4\n'+
+'Z8xZNutLDxMU9JPl+B7hWsTkyGRrhUuqUwJAUdbwe+X1xuHgXrrCxbFbFj3LEfhV\n'+
+'UAFnqYMFiCFOaFLF3racjmgUPIdMzwOXS+x2H+SpvFy4dw3fyo7w6dg87wJAGbdg\n'+
+'J0sfOzZawOL0C0HRdUWoW+RAJjrm4HogJTKZZxrnmMYCGnmhR1wXDX+UxPQLFNJI\n'+
+'tWPRGVKaNnvaxkVGiwJBAJtYEcxaI6myXbVbMdQXxvH7WISV3KMyYqP8++BkkwCs\n'+
+'O5jBcbKf9MjnneMnlvR6PBqruWt+BVUwujvUXcuPoiM=\n'+
+'-----END RSA PRIVATE KEY-----');
 
 //Allow input from the user
 const readline = require('readline');
@@ -37,16 +55,21 @@ client.on('connect', function ()
 	});
 });
 
+//Determines if the controller is still connected to the PiCar
 async function connection()
 {
+	//First set connection to false
 	isConnected = false;
-	client.publish("sub", "", function (err)
+	//Send a sub/ack request to the controller
+	client.publish('sub', key.encrypt('sub'), function (err)
 	{
 		if (err) throw "Error Description: " + err;
 	});
 	
+	//Wait 1 sec for a response
 	await sleep(1000);
 	
+	//If no ack, disable all inputs to the controller
 	if (isConnected == false)
 	{
 		process.stdin.pause();
@@ -54,15 +77,18 @@ async function connection()
 	}
 };
 
+//Require connection sub/ack every 10 seconds
 setInterval (function()
 {
 	connection();
 },10000);
 
+//Recieve sub/ack from the PiCar
 client.on('message', function(topic, message)
 {
 	if(topic == 'ack')
 	{
+		//Recieved ack from the PiCar, restore all inputs for the controller
 		if(isConnected == false)
 		{
 			console.log("Connected");
@@ -73,19 +99,21 @@ client.on('message', function(topic, message)
 });
 
 //Send data to the broker based on user input
-process.stdin.on('keypress', async function(key, data) 
+process.stdin.on('keypress', async function(keyPress, data) 
 {
 	if (data.name === 'w')
 	{
-		client.publish("data", "foward", function (err)
+		//Send encrypted foward to the PiCar
+		client.publish('data', key.encrypt('foward'), function (err)
         {
-			console.log("Sending W");
+			console.log("Sending Foward");
             if (err) throw "Error Description: " + err;
         });
 	}
 	else if (data.name === 'space')
 	{
-		client.publish("data", "stop", function (err)
+		//Send encrypted stop to the PiCar
+		client.publish('data', key.encrypt('stop'), function (err)
         {
 			console.log("Sending Stop");
             if (err) throw "Error Description: " + err;
@@ -93,41 +121,47 @@ process.stdin.on('keypress', async function(key, data)
 	}
 	else if (data.name === 's') 
 	{
-		client.publish("data", "reverse", function (err)
+		//Send encrypted reverse to the PiCar
+		client.publish('data', key.encrypt('reverse'), function (err)
         {
-			console.log("Sending S");
+			console.log("Sending Reverse");
             if (err) throw "Error Description: " + err;
         });
 	}
 	else if (data.name === 'a') 
 	{
-		client.publish("data", "left", function (err)
+		//Send encrypted left to the PiCar
+		client.publish('data', key.encrypt('left'), function (err)
         {
-			console.log("Sending R");
+			console.log("Sending Left");
             if (err) throw "Error Description: " + err;
         });
 	}
 	else if (data.name === 'd') 
 	{
-		client.publish("data", "right", function (err)
+		//Send encrypted right to the PiCar
+		client.publish('data', key.encrypt('right'), function (err)
         {
-			console.log("Sending D");
+			console.log("Sending Right");
             if (err) throw "Error Description: " + err;
         });
 	}
 	else if (data.ctrl && data.name === 't')
 	{
-		client.publish("data", "exit", async function (err)
+		//Send encrypted exit to the PiCar
+		client.publish('data', key.encrypt('exit'), async function (err)
 		{
 			console.log("Sending Exit");
 			if (err) throw "Error Description: " + err;
 		});
+		//Close out controller after the PiCar has been exited
 		console.log("Exiting");
 		await sleep(1000)
 		process.exit();
 	}
 });
 
+//Sleep function to allow time between publishing
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
